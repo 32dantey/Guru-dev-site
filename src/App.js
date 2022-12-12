4f460f90578c5d59d66f86ac960e048dcf1df69f
32dantey <nndurere32@gmail.com>
2022-12-12 23:47:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero'
+import About from './components/About';
+import Support from './components/Support';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+import Work from './components/Work';
+
+import {Routes, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    
+
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <Pricing />
+      <Footer />
+    </div>
+
+    
+    
+  );
+}
+
+export default App;
